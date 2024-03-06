ba0aeef362f3c786897cc491971c1ae8d98ca300
aheedshah <aheed@duck.com>
2024-03-06 11:32:54 +0000

    new solution

---
+++ 141 Linked List Cycle/Solution.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let fastPointer = head;
+    while(fastPointer && fastPointer.next) {
+        head = head.next;
+        fastPointer = fastPointer.next.next;
+        if(head === fastPointer) {
+            return true;
+        }
+    }
+    return false;
+};
